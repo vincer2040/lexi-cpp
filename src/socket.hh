621c16810756502f69bf0afbe9bccddb60da494e
boreddad <ronnebaumfinances@gmail.com>
2023-09-26 19:41:56 -0400

    feat: socket class

---
+++ src/socket.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <sys/types.h>
+#include <tuple>
+
+class Socket {
+  public:
+    Socket(std::string addr_str, std::uint16_t port);
+    int create_connection(void);
+    ssize_t send(std::uint8_t* send_buf, std::size_t send_len);
+    std::tuple<std::uint8_t*, ssize_t> receive(void);
+    ~Socket(void);
+
+  private:
+    std::uint32_t addr;
+    std::uint16_t port;
+    std::size_t read_buf_cap;
+    std::uint8_t* read_buf;
+    int fd;
+    int allocate_read_buf(void);
+    int reallocate_buf(std::size_t cur_pos);
+};
