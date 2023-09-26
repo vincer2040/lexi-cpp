f8d3280e29def96109c681ab83a340855cbdbe5c
boreddad <ronnebaumfinances@gmail.com>
2023-09-26 12:52:55 -0400

    test: lex bulk strings

--- tests/lexer_test.cc
+++ tests/lexer_test.cc
@@ -7,14 +7,14 @@ TEST(Lexer, BulkStrings) {
     std::uint8_t* input = (std::uint8_t*)"$5\r\nvince\r\n";
     Lexer l = Lexer(input, 11);
     Token exps[] = {
-        { TokenT::BulkType, std::monostate() },
-        { TokenT::Len, "5" },
-        { TokenT::Retcar, std::monostate() },
-        { TokenT::NewL, std::monostate() },
-        { TokenT::Bulk, "vince" },
-        { TokenT::Retcar, std::monostate() },
-        { TokenT::NewL, std::monostate() },
-        { TokenT::Eof, std::monostate() },
+        {TokenT::BulkType, std::monostate()},
+        {TokenT::Len, "5"},
+        {TokenT::Retcar, std::monostate()},
+        {TokenT::NewL, std::monostate()},
+        {TokenT::Bulk, "vince"},
+        {TokenT::Retcar, std::monostate()},
+        {TokenT::NewL, std::monostate()},
+        {TokenT::Eof, std::monostate()},
     };
     std::size_t i, len = sizeof(exps) / sizeof(exps[0]);
 
@@ -22,5 +22,14 @@ TEST(Lexer, BulkStrings) {
         Token exp = exps[i];
         Token got = l.next_token();
         EXPECT_EQ(exp.type, got.type);
+        if ((exp.type == TokenT::Len) || (exp.type == TokenT::Bulk)) {
+            EXPECT_EQ(std::holds_alternative<std::string>(exp.literal), true);
+            std::string exp_val = std::get<std::string>(exp.literal);
+            std::string got_val = std::get<std::string>(got.literal);
+            EXPECT_EQ(exp_val, got_val);
+        } else {
+            EXPECT_EQ(std::holds_alternative<std::monostate>(exp.literal),
+                      true);
+        }
     }
 }
