7339d89eb3ece538763cd18f0a3fd638f36f1bd3
boreddad <ronnebaumfinances@gmail.com>
2023-09-26 21:30:56 -0400

    feat: cluster commands

--- src/lexi.hh
+++ src/lexi.hh
@@ -15,6 +15,14 @@ class Lexi {
     LexiType set(std::string key, std::int64_t data);
     LexiType get(std::string key);
     LexiType del(std::string key);
+    LexiType cluster_new(std::string cluster_name);
+    LexiType cluster_set(std::string cluster_name, std::string key,
+                         std::string data);
+    LexiType cluster_set(std::string cluster_name, std::string key,
+                         std::int64_t value);
+    LexiType cluster_get(std::string cluster_name, std::string key);
+    LexiType cluster_del(std::string cluster_name, std::string key);
+    LexiType cluster_drop(std::string cluster_name);
     ~Lexi(void);
 
   private:
