8c69ff2f111768ccba4018901f4f9c482551251f
boreddad <ronnebaumfinances@gmail.com>
2024-01-12 17:05:55 -0500

    feat: give some hint to type when logging

--- src/lexi_data.cc
+++ src/lexi_data.cc
@@ -6,7 +6,7 @@ std::ostream& operator<<(std::ostream& stream, lexi_data& data) {
     case lexi_data_type::Invalid:
         stream << "";
         break;
-    case lexi_data_type::Simple:{
+    case lexi_data_type::Simple: {
         simple_string simple = std::get<simple_string>(data.data);
         switch (simple) {
         case simple_string::Ok:
@@ -20,16 +20,17 @@ std::ostream& operator<<(std::ostream& stream, lexi_data& data) {
             break;
         }
     } break;
-    case lexi_data_type::String:{
+    case lexi_data_type::String: {
         std::string& s = std::get<std::string>(data.data);
-        stream << s;
+        stream << "\"" << s << "\"";
     } break;
-    case lexi_data_type::Int:{
+    case lexi_data_type::Int: {
         int64_t integer = std::get<int64_t>(data.data);
-        stream << integer;
+        stream << "(int) " << integer;
     } break;
-    case lexi_data_type::Array:{
-        std::vector<lexi_data>& arr = std::get<std::vector<lexi_data>>(data.data);
+    case lexi_data_type::Array: {
+        std::vector<lexi_data>& arr =
+            std::get<std::vector<lexi_data>>(data.data);
         size_t i, len = arr.size();
         for (i = 0; i < len; ++i) {
             if (i != len - 1) {
@@ -39,7 +40,7 @@ std::ostream& operator<<(std::ostream& stream, lexi_data& data) {
             }
         }
     } break;
-    case lexi_data_type::Error:{
+    case lexi_data_type::Error: {
         std::string& e = std::get<std::string>(data.data);
         stream << e;
     } break;
